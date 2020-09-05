42542046d5165a4c256b386102d3cf78f721498a
Nelson Chaves <nelsonchavespro@gmail.com>
2020-09-05 10:22:27 -0700

    languages model method

--- app/models/course.rb
+++ app/models/course.rb
@@ -13,14 +13,7 @@ class Course < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  # friendly_id :generated_slug, use: :slugged
-  # def generated_slug
-  #   require 'securerandom'
-  #   @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
-  # end
-  #
-  # def to_s
-  #   slug
-  # end
-
+  LANGUAGES = [ "English", "Russian", "Portuguese", "Spanish"]
+  def self.languages
+    LANGUAGES.map { |language| [ language, language ] }
 end
