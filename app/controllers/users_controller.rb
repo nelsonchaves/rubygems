e9c30c95f580ee8aaa234a46e48bbc0c7940a36e
Nelson Chaves <nelsonchavespro@gmail.com>
2020-09-04 19:00:17 -0700

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.order(created_at: :desc)
+    # @users = User.all.order(created_at: :desc)
+
+    @q = Person.ransack(params[:q])
+    @people = @q.result(distinct: true)
   end
 end
