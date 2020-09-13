4dfb7d216dbc5313c52f6ce8b1961c24b1c54901
Nelson Chaves <nelsonchavespro@gmail.com>
2020-09-12 20:55:49 -0700

    Rolify Gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
 
+  rolify
+
   def to_s
     email
   end
@@ -13,5 +15,11 @@ class User < ApplicationRecord
   end
 
   has_many :courses
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:student) if self.roles.blank?
+  end
 
 end
