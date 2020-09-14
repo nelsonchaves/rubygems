ecedeb742b8c27f907bf17a3cba1b21b92914829
Nelson Chaves <nelsonchavespro@gmail.com>
2020-09-13 20:58:30 -0700

    added update to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,12 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User #{@user.email} was successfully updated.'
+    else
+      render :edit
+    end
+  end
 end
