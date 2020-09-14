84d621529ebc4da73b0fb664bc825111cff5af52
Nelson Chaves <nelsonchavespro@gmail.com>
2020-09-13 20:32:06 -0700

    def edit controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,8 @@ class UsersController < ApplicationController
     @q = User.ransack(params[:q])
     @users = @q.result(distinct: true)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 end
