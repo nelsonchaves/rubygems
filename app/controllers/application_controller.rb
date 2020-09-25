cd5aa7fb4ba0dcc16aaec7c6a7bf02ac5fd41577
Nelson Chaves <nelsonchavespro@gmail.com>
2020-09-25 15:53:55 -0700

    Added styling for users online

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  after_action :user_activity
+
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   include PublicActivity::StoreController #save current_user using gem PublicActivity
 
   before_action :set_global_variables, if: :user_signed_in?
-
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
   end
 
   private
+
+  def user_activity
+    current_user.try :touch
+  end
 
   def user_not_authorized #Pundit
     flash[:alert] = "You are not authorized to perform this action."
