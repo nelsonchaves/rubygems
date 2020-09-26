62456ed84bb6ffd966d5c430d9a597bd4750e26c
Nelson Chaves <nelsonchavespro@gmail.com>
2020-09-25 22:07:50 -0700

    breadcrumbs created and rich text area

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,7 +2,16 @@ class Lesson < ApplicationRecord
   belongs_to :course
   validates :title, :content, :course, presence: true
 
+  has_rich_text :content
+
   extend FriendlyId
   friendly_id :title, use: :slugged
-  
+
+  def to_s
+    title
+  end
+
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
 end
