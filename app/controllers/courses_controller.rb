531aa8e93f84a3c52111b6b848df647bce096662
Nelson Chaves <nelsonchavespro@gmail.com>
2020-11-18 17:09:57 -0800

    finished methods for teacher and student

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -9,6 +9,8 @@ class CoursesController < ApplicationController
     #   @q = Course.ransack(params[:q])
     #   @courses = @q.result.includes(:user)
     # end
+    @ransack_path = courses_path
+
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
     # @courses = @ransack_courses.result.includes(:user)
 
@@ -16,17 +18,23 @@ class CoursesController < ApplicationController
   end
 
   def purchased
-    @pagy, @courses = @pagy(Course.joins(:enrollments).where(enrollments: {user: current_user}))
+    @ransack_path = purchased_courses_path
+    @ransack_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).ransack(params[:courses_search], search_key: :courses_search)
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
     render 'index'
   end
 
   def pending_review
-    @pagy, @courses = @pagy(Course.joins(:enrollments).merge(Enrollment.pending_review.where(user: current_user)))
+    @ransack_path = pending_review_courses_path
+    @ransack_courses = Course.joins(:enrollments).merge(Enrollment.pending_review.where(user: current_user)).ransack(params[:courses_search], search_key: :courses_search)
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
     render 'index'
   end
 
   def created
-    @pagy, @courses = @pagy(Course.where(user: current_user))
+    @ransack_path = created_courses_path
+    @ransack_courses = Course.where(user: current_user).ransack(params[:courses_search], search_key: :courses_search)
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
     render 'index'
   end
 
--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -6,6 +6,8 @@ class EnrollmentsController < ApplicationController
     # @enrollments = Enrollment.all
     # @pagy, @enrollments = pagy(Enrollment.all)
 
+    @ransack_path = enrollments_path
+
     @q = Enrollment.ransack(params[:q])
     @pagy, @enrollments = pagy(@q.result.includes(:user))
 
@@ -13,7 +15,9 @@ class EnrollmentsController < ApplicationController
   end
 
   def my_students
-    @enrollments = Enrollment.joins(:course).where(courses: {user: current_user})
+    @ransack_path = my_students_enrollments_path
+    @q = Enrollment.joins(:course).where(courses: {user: current_user}).ransack(params[:q])
+    @pagy, @enrollments = pagy(@q.result.includes(:user))
     render 'index'
   end
 
