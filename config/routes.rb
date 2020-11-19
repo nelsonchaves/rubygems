afa363034348129f137dc3e9a3fc7811cb11d390
Nelson Chaves <nelsonchavespro@gmail.com>
2020-11-18 16:22:35 -0800

    added pending review to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :enrollments
   devise_for :users
   resources :courses do
-    get :purchased, on: :collection
+    get :purchased, :pending_review, on: :collection
     resources :lessons
     resources :enrollments, only: [:new, :create]
   end
