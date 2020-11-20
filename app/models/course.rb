684daf0b4b5b83244330ad0a072d928fc055123b
Nelson Chaves <nelsonchavespro@gmail.com>
2020-11-19 18:37:00 -0800

    created titles and new styling

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,9 +3,11 @@ class Course < ApplicationRecord
   validates :description, presence: true, length: { :minimum => 5 }
 
   belongs_to :user, counter_cache: true
-  #User.find_each { |user| User.reset_counters(user.id, :courses) }  
+  #User.find_each { |user| User.reset_counters(user.id, :courses) }
   has_many :lessons, dependent: :destroy
   has_many :enrollments
+
+  validates :title, uniqueness: true
 
   def to_s
     title
