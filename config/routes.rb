5bd9f3e4b6776718ba0dfeaf16f12f1f2db4dfa2
Nelson Chaves <nelsonchavespro@gmail.com>
2020-12-27 09:59:26 -0800

    namespace charts

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,36 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
+  root 'home#index'
+  get 'home/index'
+  get 'activity', to: 'home#activity'
+  get 'analytics', to: 'home#analytics'
+
+
+
+
   resources :enrollments do
     get :my_students, on: :collection
   end
-  devise_for :users
+
   resources :courses do
     get :purchased, :pending_review, :created, on: :collection
     resources :lessons
     resources :enrollments, only: [:new, :create]
   end
+
   resources :users, only: [:index, :edit, :show, :update]
-  get 'home/index'
-  get 'activity', to: 'home#activity'
-  get 'analytics', to: 'home#analytics'
-  root 'home#index'
+
+  namespace :charts do
+    get 'users_per_day'
+    get 'enrollments_per_day'
+    get 'course_popularity'
+    get 'money_makers'
+  end
+
+  # get 'charts/users_per_day', to: 'charts#users_per_day'
+  # get 'charts/enrollments_per_day', to: 'charts#enrollments_per_day'
+  # get 'charts/course_popularity', to: 'charts#course_popularity'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
