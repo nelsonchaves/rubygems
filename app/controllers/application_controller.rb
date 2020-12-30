da6cae4e7e9dabbe410207a8756aeaee7518a89e
Nelson Chaves <nelsonchavespro@gmail.com>
2020-12-29 20:18:00 -0800

    pause

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  after_action :user_activity
+  after_action :user_activity, if: :user_signed_in?
 
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
